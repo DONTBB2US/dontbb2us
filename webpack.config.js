b0a9d04cf36a6680da15780a813c21754616f358
shawn <shawn>
2017-03-30 23:29:57 +0800

    学习 webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 var path = require('path');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    entry: './app/index.js',
+    entry: {
+        "app": './app/index.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
+        publicPath: '/',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html'
+        })
+    ]
 }
